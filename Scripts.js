4f3a881aaa94a29796cdcddaa1d50f300842b6bf
TheWhiteNakk <leb.marco@gmail.com>
2025-03-20 01:10:08 -0400

    Create Scripts.js

---
+++ Scripts.js
@@ -0,0 +1,8 @@
+// Example of button hover effect or any interactivity you might want
+document.querySelector('button').addEventListener('mouseover', () => {
+    document.querySelector('button').style.backgroundColor = '#218838';
+});
+
+document.querySelector('button').addEventListener('mouseout', () => {
+    document.querySelector('button').style.backgroundColor = '#28a745';
+});
